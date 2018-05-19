d3ab0eab94bc9eca81a64e33fa411aecde62d5e5
MichaelSosin <mihail.ms93@gmail.com>
2018-05-19 18:25:23 +0300

    add index file with hardcoded data

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,2 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
+
+const pair = ['Trainspotting', '28 Days Later']
+
+ReactDOM.render(
+  <App pair={pair}/>,
+  document.getElementById('app')
+)
